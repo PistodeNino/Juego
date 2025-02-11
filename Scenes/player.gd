61094741597f81542b2341736efa3608b152ae1b
PistodeNino <santisainzuk@gmail.com>
2025-02-11 13:45:45 +0100

    disparo y bola implementada

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,19 +1,11 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SHOOT = preload("res://Scenes/shoot.tscn")
 
-var firing = false
+
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("fire") and not firing:
-		firing = true
-		print("disparo")
-		$AnimatedSprite2D.stop()
-		$AnimatedSprite2D.play("fire", true)
-	elif Input.is_action_just_released("fire"):
-		firing = false
-	
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction != 0:
 		velocity.x = direction * SPEED
@@ -24,5 +16,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if not $AnimatedSprite2D.is_playing() or $AnimatedSprite2D.animation != "Idle":
 			$AnimatedSprite2D.play("Idle")
+	if Input.is_action_just_pressed("fire"):
+		$AnimatedSprite2D.play("fire")	
+		fire()
+	
 
 	move_and_slide()
+	
+func fire():
+	var shoot = SHOOT.instantiate()
+	shoot.position = position - Vector2(0, 120)
+	get_parent().add_child(shoot)
