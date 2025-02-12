4c4588079c64ba1bde9e9488d445bbf90ee41565
PistodeNino <santisainzuk@gmail.com>
2025-02-12 09:31:34 +0100

    colisiones ajustadas (excepto disparo)

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -1,9 +1,9 @@
 extends RigidBody2D
 
-@export var bounciness: float = 0.8  # Cuánto rebota la bola (0 = sin rebote, 1 = rebote completo)
+@export var bounciness: float = 1  
 
 func _ready():
 	# Configurar la física
 	physics_material_override = PhysicsMaterial.new()
-	physics_material_override.bounce = bounciness  # Ajusta el rebote
-	physics_material_override.friction = 0.2  # Fricción para que no se deslice demasiado
+	physics_material_override.bounce = bounciness 
+	physics_material_override.friction = 0
