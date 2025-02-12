ab72dfa1af5d56fe77095a4fc2aa4be48aaf258d
PistodeNino <santisainzuk@gmail.com>
2025-02-12 12:54:16 +0100

    temporizador del disparo perfecto

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const SHOOT = preload("res://Scenes/shoot.tscn")
 
-
+var can_shoot = true
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
@@ -16,14 +16,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if not $AnimatedSprite2D.is_playing() or $AnimatedSprite2D.animation != "Idle":
 			$AnimatedSprite2D.play("Idle")
-	if Input.is_action_just_pressed("fire"):
-		$AnimatedSprite2D.play("fire")	
+	
+	if Input.is_action_just_pressed("fire") and can_shoot:
+		$AnimatedSprite2D.play("fire")
 		fire()
-	
-	
+
 	move_and_slide()
-	
+
 func fire():
+	can_shoot = false
+	
 	var shoot = SHOOT.instantiate()
 	shoot.position = position - Vector2(0, 120)
 	get_parent().add_child(shoot)
+
+	
+	shoot.bullet_freed.connect(Callable(self, "_on_bullet_free"))
+
+func _on_bullet_free():
+	can_shoot = true
