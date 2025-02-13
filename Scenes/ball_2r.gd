8d509acf4f16faa32b643f54f2aeffe0c5a74f01
PistodeNino <santisainzuk@gmail.com>
2025-02-13 09:33:14 +0100

    todas las bolas terminadas y físicas aceptables

---
+++ Scenes/ball_2r.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+@export var bounciness: float = 1  
+@export var lateral_force: float = -500  
+
+func _ready():
+	add_to_group("medium")
+	physics_material_override = PhysicsMaterial.new()
+	physics_material_override.bounce = bounciness 
+	physics_material_override.friction = 0
+	
+	
+	linear_velocity = Vector2(lateral_force, 0)  
+	
+	
